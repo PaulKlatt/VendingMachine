d35d1e47f85d4bc2d9988d1d5f9cbfeb8f05f1b5
Paul Klatt <bcingul8r@gmail.com>
2021-10-07 15:04:31 -0400

    VendingMachine class properties and Restock method added

---
+++ dotnet/Capstone/Classes/VendingMachine/VendingMachine.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone.Classes.VendingMachineItems;
+using System.IO;
+
+namespace Capstone.Classes.VendingMachine
+{
+    public class VendingMachine
+    {
+        public Dictionary<string, VendingMachineItem> Inventory { get; set; }
+
+        public decimal CurrentBalance { get; set; }
+
+        public decimal TotalSales { get; set; }
+
+        public void Restock(string inputFilePath)
+        {
+            try
+            {
+                using(StreamReader sr = new StreamReader(inputFilePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = Console.ReadLine();
+                        string[] itemParts = line.Split("|");
+                        string slot = itemParts[0];
+                        string productName = itemParts[1];
+                        decimal productCost = decimal.Parse(itemParts[2]);
+                        string productType = itemParts[3];
+
+                        if (productType == "Chip")
+                        {
+                            Chips newChips = new Chips(productName, productCost, slot);
+                            Inventory[slot] = newChips;
+                        }
+                        else if (productType == "Candy")
+                        {
+                            Candy newCandy = new Candy(productName, productCost, slot);
+                            Inventory[slot] = newCandy;
+                        }
+                        else if (productType == "Gum")
+                        {
+                            Gum newGum = new Gum(productName, productCost, slot);
+                            Inventory[slot] = newGum;
+                        }
+                        else if (productType == "Beverage")
+                        {
+                            Beverage newBeverage = new Beverage(productName, productCost, slot);
+                            Inventory[slot] = newBeverage;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("An error occurred restocking machine..." + ex.Message);
+            }
+        }
+    }
+}
